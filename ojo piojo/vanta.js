1c790c06fe97252061bec711e0e58344dd5329f9
angeandueza <142035763+angeandueza@users.noreply.github.com>
2024-05-16 19:41:45 -0400

    ojopiojo

---
+++ ojo piojo/vanta.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    VANTA.NET({
+        el: "#vanta-bg",
+        color: 12436203,
+        backgroundColor: 1973792,
+        points: 10,
+        spacing: 15,
+        maxDistance: 20,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        minHeight: 200,
+        minWidth: 200,
+        mouseControls: false,
+        touchControls: false,
+        gyroControls: false
+    });
+});
